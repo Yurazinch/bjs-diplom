940b37badf355f69873706f390d1b229013c8335
Yuriy Zinchenko <yurazinch@gmail.com>
2025-02-28 20:34:20 +0300

    bjs-diplom-homePage_currentUser

--- public/homePage.js
+++ public/homePage.js
@@ -1,4 +1,21 @@
+const { response } = require("express");
+
 // объект для выхода из личного кабинета
 const logoutUser = new LogoutButton();
 
-logoutUser.action = logout()
+logoutUser.action = (callback) => {
+    ApiConnector.logout(callback, (response) => {
+        if(response.success) {
+            location.reload();
+        }
+    })
+}
+
+// получение информации о пользователе
+ApiConnector.current(callback, (response) => {
+    if(response.success) {
+        ProfileWidget.showProfile(response);
+    }
+});
+
+    
